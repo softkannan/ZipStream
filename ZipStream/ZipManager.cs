bd01f833051513e634ff1a90278221399815f87a
softkannan <softkannan@users.noreply.github.com>
2018-08-20 15:29:55 -0500

    Created

---
+++ ZipStream/ZipManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZipStream
+{
+    public class ZipManager
+    {
+        public const string Zip = "-zip";
+        public const string UnZip = "-unzip";
+        public const string Type = "-gz";
+
+        private CompressionAction _action = CompressionAction.Zip;
+        private CompressionAlg _compressionAlg = CompressionAlg.Gz;
+
+        public string SourceFile { get; set; }
+        public string DestFile { get; set; }
+
+        public ZipManager()
+        {
+            SourceFile = string.Empty;
+            DestFile = string.Empty;
+            ParseCommandline(Environment.GetCommandLineArgs());
+        }
+
+        public void Execute()
+        {
+            if (!string.IsNullOrEmpty(SourceFile) && !string.IsNullOrEmpty(DestFile))
+            {
+                var stream = new ZipStream(SourceFile, DestFile);
+
+                if(_action == CompressionAction.Zip)
+                {
+                    stream.Compress();
+                }
+                else if(_action == CompressionAction.UnZip)
+                {
+                    stream.Decompress();
+                }
+            }
+        }
+
+        private void ParseCommandline(IEnumerable<string> commandlineArgs)
+        {
+            var enumurator = commandlineArgs.GetEnumerator();
+            enumurator.Reset();
+            //ignore first argument
+            enumurator.MoveNext();
+            while (enumurator.MoveNext())
+            {
+                string lowerCaseToken = enumurator.Current.ToLower();
+                switch (lowerCaseToken)
+                {
+                    case Zip:
+                        break;
+                    case UnZip:
+                        break;
+                    case Type:
+                        {
+                        }
+                        break;
+                    default:
+                        {
+                            if(string.IsNullOrEmpty(SourceFile))
+                            {
+                                SourceFile = Environment.ExpandEnvironmentVariables(enumurator.Current);
+                            }
+                            else if(string.IsNullOrEmpty(DestFile))
+                            {
+                                DestFile = Environment.ExpandEnvironmentVariables(enumurator.Current);
+                            }
+                        }
+                        break;
+                }
+            }
+
+            if (string.IsNullOrEmpty(SourceFile) || string.IsNullOrEmpty(DestFile))
+            {
+                MessageBox.Show("invalid options", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ ZipStream/ZipStream.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+
+namespace ZipStream
+{
+    public class ZipStream
+    {
+        //We pick a value that is the largest multiple of 4096 that is still smaller than the large object heap threshold (85K).
+        // The CopyTo/CopyToAsync buffer is short-lived and is likely to be collected at Gen0, and it offers a significant
+        // improvement in Copy performance.
+        private const int _DefaultCopyBufferSize = 81920;
+
+        private string _originalFile;
+        private string _newFile;
+
+        public ZipStream(string originalFile, string newFile)
+        {
+            _originalFile = originalFile;
+            _newFile = newFile;
+        }
+
+        public void Compress()
+        {
+            if(File.Exists(_newFile))
+            {
+                try { File.Delete(_newFile); } catch { }
+            }
+
+            using (FileStream originalFileStream = new FileStream(_originalFile, FileMode.Open,FileAccess.Read,FileShare.ReadWrite))
+            {
+                using (FileStream compressedFileStream = new FileStream(_newFile, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
+                {
+                    using (GZipStream compressionStream = new GZipStream(compressedFileStream,CompressionMode.Compress))
+                    {
+                        byte[] buffer = new byte[_DefaultCopyBufferSize];
+                        int read;
+                        while ((read = originalFileStream.Read(buffer, 0, buffer.Length)) != 0)
+                            compressionStream.Write(buffer, 0, read);
+                    }
+                }
+            }
+        }
+
+        public void Decompress()
+        {
+            if (File.Exists(_newFile))
+            {
+                try { File.Delete(_newFile); } catch { }
+            }
+
+            using (FileStream originalFileStream = new FileStream(_originalFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (FileStream decompressedFileStream = new FileStream(_newFile, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
+                {
+                    using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                    {
+                        byte[] buffer = new byte[_DefaultCopyBufferSize];
+                        int read;
+                        while ((read = decompressionStream.Read(buffer, 0, buffer.Length)) != 0)
+                            decompressedFileStream.Write(buffer, 0, read);
+                    }
+                }
+            }
+        }
+    }
+}
